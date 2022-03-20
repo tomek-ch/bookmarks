862bd1ffefdc1d49c88d53bbc740310ebf0c67fd
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-03-20 10:53:34 +0100

    Fix svg attribute casing

--- components/BookMarkIcon.tsx
+++ components/BookMarkIcon.tsx
@@ -7,9 +7,9 @@ export const BookMarkIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
     </svg>
