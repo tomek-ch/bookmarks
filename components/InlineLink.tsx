67925f6d4ad98e5e9d844f44c1edfde3f0035f1e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-03-20 16:07:20 +0100

    Add click animation to inline link

--- components/InlineLink.tsx
+++ components/InlineLink.tsx
@@ -9,7 +9,7 @@ interface InlineLinkProps {
 export const InlineLink = ({ to, children }: InlineLinkProps) => {
   return (
     <Link href={to}>
-      <a className="text-blue-600 hover:text-blue-700 transition-all">
+      <a className="inline-block text-blue-600 hover:text-blue-700 active:scale-95 transition-all">
         {children}
       </a>
     </Link>
