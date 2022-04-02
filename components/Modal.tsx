af8b413aa622b3bc9747ed03ac6a793f1f665d63
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-04-02 21:45:50 +0200

    Add animation to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -8,10 +8,14 @@ interface ModalProps {
 export const Modal = ({ children, toggle }: ModalProps) => {
   return (
     <>
-      <div onClick={toggle} className="fixed inset-0 bg-black/70 w-full" />
+      <div
+        onClick={toggle}
+        className="fixed inset-0 bg-black/70 w-full animate-fade-in"
+      />
       <div className="fixed p-2 w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div
           className="
+          animate-pop-up
           p-4 bg-white w-full
           rounded-md shadow-md
           "
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        "fade-out": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+        "pop-up": {
+          "0%": { opacity: 0, transform: "scale(0.9)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+        hide: {
+          "0%": { opacity: 1, transform: "scale(1)" },
+          "100%": { opacity: 0, transform: "scale(0.9)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 150ms",
+        "pop-up": "pop-up 150ms",
+      },
+    },
   },
   plugins: [],
 };
