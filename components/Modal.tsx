66b4636ff033682f3650a29edce9e68132dd777e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-04-02 14:35:58 +0200

    Handle modal toggle

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,12 +2,13 @@ import { ReactNode } from "react";
 
 interface ModalProps {
   children: ReactNode;
+  toggle: () => void;
 }
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, toggle }: ModalProps) => {
   return (
     <>
-      <div className="fixed inset-0 bg-black/70 w-full" />
+      <div onClick={toggle} className="fixed inset-0 bg-black/70 w-full" />
       <div className="fixed p-2 w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div
           className="
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import { BookmarkItem } from "../components/bookmarks/BookmarkItem";
 import { FloatingButton } from "../components/common/buttons/FloatingButton";
 import { PlusIcon } from "../components/icons/PlusIcon";
+import { Modal } from "../components/Modal";
+import { useModal } from "../hooks/useModal";
 import { Bookmark } from "../types/Bookmark";
 
 interface HomeProps {
@@ -9,15 +11,17 @@ interface HomeProps {
 }
 
 const Home: NextPage<HomeProps> = ({ bookmarks }) => {
+  const { isOpen, toggle } = useModal();
   return (
     <div className="pb-8">
       <h1 className="text-xl mb-4">Your bookmarks:</h1>
       {bookmarks.map((item) => (
         <BookmarkItem key={item.id} {...item} />
       ))}
-      <FloatingButton>
+      <FloatingButton onClick={toggle}>
         <PlusIcon className="scale-150" />
       </FloatingButton>
+      {isOpen && <Modal toggle={toggle}>HELLO</Modal>}
     </div>
   );
 };
