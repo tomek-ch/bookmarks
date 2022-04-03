b5cb56066fce17b15de9e1de4c89a03547f4532a
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-04-03 21:52:35 +0200

    Remove unnecessary elements

--- components/auth/LogInForm.tsx
+++ components/auth/LogInForm.tsx
@@ -5,29 +5,27 @@ import { Button } from "../common/buttons/Button";
 import { InputField } from "../common/InputField";
 
 export const LogInForm = () => (
-  <div>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      validationSchema={object({
-        email: string()
-          .required("Please enter your email address")
-          .email("Invalid email address"),
-        password: string().required("Please provide a password"),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className="flex flex-col gap-3 items-start">
-          <InputField type="email" name="email" label="Email" />
-          <InputField type="password" name="password" label="Password" />
-          <Button variant="primary" disabled={isSubmitting}>
-            Sign in
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  </div>
+  <Formik
+    initialValues={{ email: "", password: "" }}
+    validationSchema={object({
+      email: string()
+        .required("Please enter your email address")
+        .email("Invalid email address"),
+      password: string().required("Please provide a password"),
+    })}
+    onSubmit={(values, { setSubmitting }) => {
+      console.log(values);
+      setSubmitting(false);
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form className="flex flex-col gap-3 items-start">
+        <InputField type="email" name="email" label="Email" />
+        <InputField type="password" name="password" label="Password" />
+        <Button variant="primary" disabled={isSubmitting}>
+          Sign in
+        </Button>
+      </Form>
+    )}
+  </Formik>
 );
--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -5,31 +5,29 @@ import { Button } from "../common/buttons/Button";
 import { InputField } from "../common/InputField";
 
 export const RegisterForm = () => (
-  <div>
-    <Formik
-      initialValues={{ name: "", email: "", password: "" }}
-      validationSchema={object({
-        name: string().required("Please enter your name"),
-        email: string()
-          .required("Please enter your email address")
-          .email("Invalid email address"),
-        password: string().required("Please provide a password"),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className="flex flex-col gap-3 items-start">
-          <InputField name="name" label="Name" />
-          <InputField type="email" name="email" label="Email" />
-          <InputField type="password" name="password" label="Password" />
-          <Button variant="primary" disabled={isSubmitting}>
-            Sign up
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  </div>
+  <Formik
+    initialValues={{ name: "", email: "", password: "" }}
+    validationSchema={object({
+      name: string().required("Please enter your name"),
+      email: string()
+        .required("Please enter your email address")
+        .email("Invalid email address"),
+      password: string().required("Please provide a password"),
+    })}
+    onSubmit={(values, { setSubmitting }) => {
+      console.log(values);
+      setSubmitting(false);
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form className="flex flex-col gap-3 items-start">
+        <InputField name="name" label="Name" />
+        <InputField type="email" name="email" label="Email" />
+        <InputField type="password" name="password" label="Password" />
+        <Button variant="primary" disabled={isSubmitting}>
+          Sign up
+        </Button>
+      </Form>
+    )}
+  </Formik>
 );
