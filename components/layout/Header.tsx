b9a4288de51d78f179e39db9059176779347747a
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-07-16 15:23:42 +0200

    Hide header when authenticating

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import { useQuery } from "react-query";
+import { validate } from "../../api/api";
 import { Logo } from "../common/Logo";
 import { UserNav } from "../layout/UserNav";
 
 export const Header = () => {
+  const { isLoading } = useQuery("currentUser", validate);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <header className="py-3 items-center flex justify-between">
       <Link href="/">
